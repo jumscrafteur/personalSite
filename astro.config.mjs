f7ca01aefd351d7a95ed256a0a6a1bcfcbbc1a35
Hugo Sansané <hsansane@gmail.com>
2024-12-15 16:51:28 +0100

    Migration Astro icons & Ajout Logs Github Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), icon()],
   site: 'https://jumscrafteur.fr',
   server: {
     headers: {
